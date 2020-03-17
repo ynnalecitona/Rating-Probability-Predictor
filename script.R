2ccc8659ec3a413531af288aa9dae5a856efa5dd
Tycho Yacub <tychoyacub@gmail.com>
2020-03-16 19:28:04 -0700

    script to test for segfault2

--- script.R
+++ script.R
@@ -1,9 +1,6 @@
 source('loadData.R')
 trn <- loadData('songListTrainSet')
-trn <- trn[sample(1:nrow(trn), 10000),]
 vld <- loadData('songListValidationSet')
-vld <- vld[sample(1:nrow(vld), 10000),]
 tst <- loadData('songListTestSet')
-tst <- tst[sample(1:nrow(tst), 10000),]
 source('crossValidate.R')
 x <- crossValidate(trn, vld, tst, 5, "NMF", FALSE, FALSE)
